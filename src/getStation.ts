006db023b37798bc87ef1cc062877c55951ccfdb
caldvs <callum.davies@bbc.co.uk>
2022-06-30 11:19:31 +0100

    Refactor getStation

--- src/getStation.ts
+++ src/getStation.ts
@@ -3,8 +3,8 @@ var _ = require('lodash');
 
 export const station = async (stationId: string) => {
     const data = await fetch();
-    const pickDestinationAndWait = (filtered) => {
-        return filtered.map((pid) => {
+    const extractDestinationsAndWaits = (filteredByStationId) => {
+        return filteredByStationId.map((pid) => {
             const range = [0,1,2,3];
             const dests = Array.from(range, x => `Dest${x}`)
             const waits = Array.from(range, x => `Wait${x}`)
@@ -18,22 +18,23 @@ export const station = async (stationId: string) => {
             })
         }).flat().filter(Boolean);
     };
-    const filtered = data.data.value.filter((station) => station.TLAREF === stationId)
-    const destinations = Array.from(new Set(filtered.map((pid) => [pid.Dest0, pid.Dest1, pid.Dest2, pid.Dest3]).flat().filter(Boolean)));
-    const messages = _.uniq(filtered.map((each) => each.MessageBoard));
+    const filteredByStationId = data.data.value.filter((station) => station.TLAREF === stationId)
+    const destinations = Array.from(new Set(filteredByStationId.map((pid) => [pid.Dest0, pid.Dest1, pid.Dest2, pid.Dest3]).flat().filter(Boolean)));
+    const messages = _.uniq(filteredByStationId.map((pid) => pid.MessageBoard));
 
-    const departuresSorted = destinations.map((destination) => {
-        const destinationsAndWaits = pickDestinationAndWait(filtered).map((departure) => {
+    const groupByDestination = destinations.map((destination) => {
+        const waitsGroupedByDestination = extractDestinationsAndWaits(filteredByStationId).map((departure) => {
             return departure.destination === destination && Number(departure.wait);
          }).filter(Boolean)
+        console.log("🚀 | file: getStation.ts | line 29 | waitsGroupedByDestination", waitsGroupedByDestination)
         return {
             destination,
-            mins: _.sortedUniq(destinationsAndWaits)
+            mins: waitsGroupedByDestination
         }
     });
     return {
         version: `metro-api-${process.env.environment}`,
-        departures: departuresSorted,
+        departures: groupByDestination,
         messages,
         firstAndLast: ['06:00', '23:59']
     }
