3d7e1edc8d05722ed477b3a617b081a6cd915ab5
caldvs <callum.davies@bbc.co.uk>
2022-07-07 14:44:50 +0100

    Add metrics

--- src/getStation.ts
+++ src/getStation.ts
@@ -1,13 +1,29 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "eu-west-2" });
+const cloudwatch = new AWS.CloudWatch({ apiVersion: "2010-08-01" });
+
 /* eslint-disable import/extensions */
 import type { StationResponse } from "./types/types";
-
 // eslint-disable-next-line import/no-unresolved
 import fetch from "./fetch";
 // eslint-disable-next-line import/no-unresolved
 import transform from "./transform";
+import { codeToDestination } from "./lib/mapping";
 
 export default async (stationId: string): Promise<StationResponse> => {
   const { data } = await fetch();
+  const params = {
+    MetricData: {
+      MetricName: "station",
+      Timestamp: new Date(),
+      Value: codeToDestination(stationId),
+    },
+    Namespace: "FrequencyMonitoring",
+  };
+  cloudwatch.putMetricData(params, (err: any, data: any) => {
+    if (err) console.log(err, err.stack); // an error occurred
+    console.log(JSON.stringify(params));
+  });
   const { departures, messages } = transform(data, stationId);
   return {
     version: `metro-api-${process.env.environment}`,
