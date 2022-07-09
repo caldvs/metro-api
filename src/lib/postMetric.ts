984906310ea74e1ab00a377e95baf10018771646
caldvs <callum.davies@bbc.co.uk>
2022-07-09 11:59:42 +0100

    Move post metric elsewhere

---
+++ src/lib/postMetric.ts
@@ -0,0 +1,44 @@
+import { codeToDestination } from "./mapping";
+
+const AWS = require("aws-sdk");
+
+AWS.config.update({ region: "eu-west-2" });
+const cloudwatch = new AWS.CloudWatch({ region: "eu-west-2" });
+
+export const postMetric = async (stationId: string) => {
+  const metric = {
+    MetricData: [
+      {
+        MetricName: "Request",
+        Dimensions: [
+          {
+            Name: "stationId",
+            Value: stationId,
+          },
+          {
+            Name: "stationName",
+            Value: codeToDestination(stationId),
+          },
+        ],
+        Timestamp: new Date(),
+        Unit: "Count",
+        Value: 1,
+      },
+    ],
+    Namespace: "my metrics",
+  };
+  try {
+    await cloudwatch
+      .putMetricData(metric, (err, data) => {
+        if (err) {
+          console.log(err, err.stack); // an error occurred
+        } else {
+          console.log(data); // successful response
+        }
+      })
+      .promise();
+  } catch (error) {
+    console.log("error putting stationId metric with params", metric);
+    console.log(error);
+  }
+};
--- src/station.ts
+++ src/station.ts
@@ -3,58 +3,18 @@
 /* eslint-disable import/no-import-module-exports */
 import type { StationResponse } from "./types/types";
 
+import { get } from "./lib/s3";
 import { fetch } from "./fetch";
 import { transform } from "./transform";
-import { get } from "./lib/s3";
+import { postMetric } from "./lib/postMetric";
 
-// eslint-disable-next-line import/no-unresolved
-import { codeToDestination } from "./lib/mapping";
-
-const AWS = require("aws-sdk");
-
-AWS.config.update({ region: "eu-west-2" });
-
-export const station = async (
-  stationId: string = "SPS"
-): Promise<StationResponse> => {
+export const station = async (stationId: string): Promise<StationResponse> => {
   const { data } = await fetch();
-  const metric = {
-    MetricData: [
-      {
-        MetricName: "Request",
-        Dimensions: [
-          {
-            Name: "stationId",
-            Value: stationId,
-          },
-          {
-            Name: "stationName",
-            Value: codeToDestination(stationId),
-          },
-        ],
-        Timestamp: new Date(),
-        Unit: "Count",
-        Value: 1,
-      },
-    ],
-    Namespace: "my metrics",
-  };
 
-  try {
-    const cloudwatch = new AWS.CloudWatch({ region: "eu-west-2" });
-    await cloudwatch
-      .putMetricData(metric, (err, data) => {
-        if (err) {
-          console.log(err, err.stack); // an error occurred
-        } else {
-          console.log(data); // successful response
-        }
-      })
-      .promise();
-  } catch (error) {
-    console.log("error putting stationId metric with params", metric);
-    console.log(error);
+  if (process.env.environment === "prod") {
+    postMetric(stationId);
   }
+
   const { departures, messages } = transform(data, stationId);
   const firstAndLast = await get(stationId);
   return {
