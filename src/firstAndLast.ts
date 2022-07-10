1486c9e6fcd3a22c7f8a666a856d0589b8d79a66
caldvs <callum.davies@bbc.co.uk>
2022-07-10 17:29:44 +0100

    wip scrape lat longs

--- src/firstAndLast.ts
+++ src/firstAndLast.ts
@@ -19,82 +19,40 @@ const generateUrl = (start: string, end: string): string => {
   return `https://tfgm.com/public-transport/tram/stops/${from}-tram/tram-schedule/${to}-tram#tram-schedule-panel`;
 };
 
-const generatePuppeteerObject = (stationId: string): PuppeteerObject => {
-  const terminus = allTerminus.filter((t) => t !== stationId);
+const generatePuppeteerObject = (stationId: string): any => {
   return {
     stationId,
     name: codeToDestination(stationId),
-    urls: terminus.map((t) => {
-      return {
-        destination: codeToDestination(t),
-        target: generateUrl(stationId, t),
-      };
-    }),
+    urls: [generateUrl(stationId, "eccles")],
   };
 };
 
 const scrapePages = async () => {
-  const oldestStation = await getOldestFile();
+  const oldestStation = "PIC";
   console.log("🚀 | Getting: ", oldestStation);
-  const { stationId, urls } = generatePuppeteerObject(oldestStation);
+  const { urls } = generatePuppeteerObject(oldestStation);
   // console.log(
   //   "🚀 | file: firstAndLast.ts | line 54 | util.inspect puppeteerObject",
   //   util.inspect(urls, false, null, true /* enable colors */)
   // );
 
   const result = {};
+  let stationLocation;
   Promise.all(
     await Promise.all(
-      urls.map(async ({ destination, target }) => {
+      urls.map(async (url) => {
         try {
-          const { data } = await axios.get(target);
+          const { data } = await axios.get(url);
           const $ = cheerio.load(data);
           const range = [0, 1, 2, 3, 4, 5, 6];
 
           range.forEach((i) => {
-            const day = $(`#first-last-${i}`);
-            const prettyDate = day.children(".first-last-day").text().trim();
-            const dayNumber = "0".concat(prettyDate.split(" ")[1]).slice(-2);
-            const month = "0"
-              .concat(String(getMonthFromString(prettyDate.split(" ")[2])))
-              .slice(-2); // month 7
-            const dateKey = `${dayNumber}-${month}`;
-            const firstTram = $(
-              `#first-last-${i} .first-last-times table tbody tr .first-last-first .first-last-departure-time`
-            ).text();
-            const lastTram = $(
-              `#first-last-${i} .first-last-times table tbody tr .first-last-last .first-last-departure-time`
-            ).text();
-            const changeLocationFirst = $(
-              `#first-last-${i} .first-last-times table tbody tr .first-last-first .first-last-route`
-            ).text();
-            const changeLocationLast = $(
-              `#first-last-${i} .first-last-times table tbody tr .first-last-last .first-last-route`
-            ).text();
-            if (result[dateKey]) {
-              result[dateKey].services[destination] = {
-                destination,
-                code: destinationToCode(destination),
-                firstTram,
-                lastTram,
-                changeLocationFirst,
-                changeLocationLast,
-              };
-            } else {
-              result[dateKey] = {
-                date: prettyDate,
-                services: {
-                  [destination]: {
-                    destination,
-                    code: destinationToCode(destination),
-                    firstTram,
-                    lastTram,
-                    changeLocationFirst,
-                    changeLocationLast,
-                  },
-                },
-              };
-            }
+            const location = $(`.station-address-link`).getAttribute("href");
+            console.log(
+              "🚀 | file: firstAndLast.ts | line 56 | location",
+              location
+            );
+            stationLocation = location;
           });
         } catch (error) {
           console.log("🚀 | file: firstAndLast.ts | line 119 | error", error);
@@ -102,7 +60,11 @@ const scrapePages = async () => {
       })
     )
   );
-  await save(stationId, result);
+  console.log(
+    "🚀 | file: firstAndLast.ts | line 40 | stationLocation",
+    stationLocation
+  );
+  // await save(stationId, result);
 };
 
 export const firstAndLast = async (): Promise<any> => {
